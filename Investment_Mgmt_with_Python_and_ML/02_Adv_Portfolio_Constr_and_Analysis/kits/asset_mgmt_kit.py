9724484e1c99d70198a83b77c5d02e9579cbacfe
ilias-m-n <ilias.matthias.nasri@gmail.com>
2023-10-22 01:30:29 +0200

    Finish Course 1 and add Course 2 of InvestMgmtPythonML

--- Investment_Mgmt_with_Python_and_ML/02_Adv_Portfolio_Constr_and_Analysis/kits/asset_mgmt_kit.py
+++ Investment_Mgmt_with_Python_and_ML/02_Adv_Portfolio_Constr_and_Analysis/kits/asset_mgmt_kit.py
@@ -306,6 +306,24 @@ def get_ffme_returns():
     rets.index = pd.to_datetime(rets.index, format="%Y%m").to_period('M')
     return rets
 
+def get_fff_returns():
+    """
+    Load the Fama-French Research Factor Monthly Dataset
+    """
+    rets = pd.read_csv("data/F-F_Research_Data_Factors_m.csv",
+                       header=0, index_col=0, na_values=-99.99)/100
+    rets.index = pd.to_datetime(rets.index, format="%Y%m").to_period('M')
+    return rets
+
+def get_fff_returns_notm():
+    """
+    Load the Fama-French Research Factor Monthly Dataset
+    """
+    rets = pd.read_csv("data/F-F_Research_Data_Factors.csv",
+                       header=0, index_col=0, na_values=-99.99)/100
+    rets.index = pd.to_datetime(rets.index, format="%Y%m").to_period('M')
+    return rets
+
 def get_hfi_returns():
     """
     Load and format the EDHEC Hedge Fund Index Returns
@@ -316,45 +334,61 @@ def get_hfi_returns():
     hfi.index = hfi.index.to_period('M')
     return hfi
 
-def get_ind_file(filetype):
+def get_ind_file(filetype, weighting="vw", n_inds=30):
     """
-    Load and format the Ken French 30 Industry Portfolios files
-    """
-    known_types = ["returns", "nfirms", "size"]
-    if filetype not in known_types:
-        sep = ','
-        raise ValueError(f'filetype must be one of:{sep.join(known_types)}')
-    if filetype is "returns":
-        name = "vw_rets"
+    Load and format the Ken French Industry Portfolios files
+    Variant is a tuple of (weighting, size) where:
+        weighting is one of "ew", "vw"
+        number of inds is 30 or 49
+    """    
+    if filetype == "returns":
+        name = f"{weighting}_rets" 
         divisor = 100
-    elif filetype is "nfirms":
+    elif filetype == "nfirms":
         name = "nfirms"
         divisor = 1
-    elif filetype is "size":
+    elif filetype == "size":
         name = "size"
         divisor = 1
-    ind = pd.read_csv(f"data/ind30_m_{name}.csv", header=0, index_col=0)/divisor
+    else:
+        raise ValueError(f"filetype must be one of: returns, nfirms, size")
+    
+    ind = pd.read_csv(f"data/ind{n_inds}_m_{name}.csv", header=0, index_col=0, na_values=-99.99)/divisor
     ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period('M')
     ind.columns = ind.columns.str.strip()
     return ind
 
-def get_ind_returns():
+def get_ind_returns(weighting="vw", n_inds=30):
     """
-    Load and format the Ken French 30 Industry Portfolios Value Weighted Monthly Returns
+    Load and format the Ken French Industry Portfolios Monthly Returns
     """
-    return get_ind_file("returns")
+    return get_ind_file("returns", weighting=weighting, n_inds=n_inds)
 
-def get_ind_nfirms():
+def get_ind_nfirms(n_inds=30):
     """
     Load and format the Ken French 30 Industry Portfolios Average number of Firms
     """
-    return get_ind_file("nfirms")
+    return get_ind_file("nfirms", n_inds=n_inds)
 
-def get_ind_size():
+def get_ind_size(n_inds=30):
     """
     Load and format the Ken French 30 Industry Portfolios Average size (market cap)
     """
-    return get_ind_file("size")
+    return get_ind_file("size", n_inds=n_inds)
+
+def get_ind_market_caps(n_inds=30, weights=False):
+    """
+    Load the industry portfolio data and derive the market caps
+    """
+    ind_nfirms = get_ind_nfirms(n_inds=n_inds)
+    ind_size = get_ind_size(n_inds=n_inds)
+    ind_mktcap = ind_nfirms * ind_size
+    if weights:
+        total_mktcap = ind_mktcap.sum(axis=1)
+        ind_capweight = ind_mktcap.divide(total_mktcap, axis="rows")
+        return ind_capweight
+    #else
+    return ind_mktcap
                          
 def get_total_market_index_returns():
     """
@@ -369,6 +403,14 @@ def get_total_market_index_returns():
     total_market_return = (ind_capweight * ind_return).sum(axis="columns")
     return total_market_return                     
 
+def get_total_market_index_returns(n_inds=30):
+    """
+    Load the 30 industry portfolio data and derive the returns of a capweighted total market index
+    """
+    ind_capweight = get_ind_market_caps(n_inds=n_inds)
+    ind_return = get_ind_returns(weighting="vw", n_inds=n_inds)
+    total_market_return = (ind_capweight * ind_return).sum(axis="columns")
+    return total_market_return
 
 
 ## CPPI
@@ -781,6 +823,12 @@ def regress(dependent_variable, explanatory_variables, alpha=True):
     lm = sm.OLS(dependent_variable, explanatory_variables).fit()
     return lm
 
+def tracking_error(r_a, r_b):
+    """
+    Returns the Tracking Error between the two return series
+    """
+    return np.sqrt(((r_a - r_b)**2).sum())
+
 def portfolio_tracking_error(weights, ref_r, bb_r):
     """
     returns the tracking error between the reference returns
@@ -825,6 +873,8 @@ def ff_analysis(r, factors):
     else:
         raise TypeError("r must be a Series or a DataFrame")
     return tilts
+
+## Backtesting: EW vs. CW
 
 def weight_ew(r, cap_weights=None, max_cw_mult=None, microcap_threshold=None, **kwargs):
     """
